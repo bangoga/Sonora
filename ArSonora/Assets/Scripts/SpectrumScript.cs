779f01b33c2c30d68877a73a89fc64b0c41d57e0
Khalil Mohsin <mohsin.khalil8@gmail.com>
2020-01-25 13:40:39 -0500

    New Setup

---
+++ ArSonora/Assets/Scripts/SpectrumScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectrumScript : MonoBehaviour
+{
+    // I have an empty object of audiolistenr that is then linked to the current speuctrum script 
+    public AudioListener Audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
