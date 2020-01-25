30fbdabaa1b637ed874956699442e8c46c61db3c
Howard Tseng <howardt12345@users.noreply.github.com>
2020-01-25 14:03:21 -0500

    test

--- ArSonora/Assets/Scripts/SpectrumScript.cs
+++ ArSonora/Assets/Scripts/SpectrumScript.cs
@@ -9,7 +9,7 @@ public class SpectrumScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.log("hello, world");
     }
 
     // Update is called once per frame
