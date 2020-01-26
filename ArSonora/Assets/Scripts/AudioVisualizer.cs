02260de42828bb814ad1540e820a73ffc6a5f60c
Howard Tseng <howardt12345@users.noreply.github.com>
2020-01-25 19:51:09 -0500

    Added generation of cubes

--- ArSonora/Assets/Scripts/AudioVisualizer.cs
+++ ArSonora/Assets/Scripts/AudioVisualizer.cs
@@ -4,23 +4,25 @@ using System.Collections;
 
 public class AudioVisualizer : MonoBehaviour
 {
-	//public GameObject prefab;
+	public GameObject prefab;
 	public int numberOfObjects = 20;
 	public GameObject[] cubes;
 	public AudioSource src;
+	public float radius = 5f;
 	public float scale = 100f;
 	public int range = 1024;
 	public int updateSpeed = 480;
 	public int rotateSpeed = 1000;
 
-	void Start () {
-		//for (int i = 0; i < numberOfObjects; i++) {
-		//float angle = i * Mathf.PI * 2 / numberOfObjects;
-		//Vector3 pos = new Vector3 (Mathf.Cos (angle), 0, Mathf.Sin (angle)) * radius;
-
-		//Instantiate (prefab, pos , Quaternion.identity);
-		//}
-		//cubes = GameObject.FindGameObjectsWithTag("GameController");
+	void Start ()
+	{
+		for (int i = 0; i < numberOfObjects; i++) {
+			float angle = i * Mathf.PI * 2 / numberOfObjects;
+			Vector3 pos = new Vector3 (Mathf.Cos (angle), 0, Mathf.Sin (angle)) * radius;
+			Instantiate(prefab, pos, Quaternion.identity);
+			
+		}
+		cubes = GameObject.FindGameObjectsWithTag("Cube");
 	}
 
 	void Update () {
