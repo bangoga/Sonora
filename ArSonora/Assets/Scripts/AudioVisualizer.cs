b33bbf639ed376fe5cdc986090e06580600611c7
Howard Tseng <howardt12345@gmail.com>
2020-01-26 10:36:02 -0500

    Removed vuforia

--- ArSonora/Assets/Scripts/AudioVisualizer.cs
+++ ArSonora/Assets/Scripts/AudioVisualizer.cs
@@ -4,42 +4,42 @@ using System.Collections;
 
 public class AudioVisualizer : MonoBehaviour
 {
-	public GameObject prefab;
-	public GameObject parent;
-	public int numberOfObjects = 64;
-	public AudioSource src;
-	public float radius = 5f;
-	public float scale = 100f;
-	public int range = 1024;
-	public int updateSpeed = 480;
-	public int rotateSpeed = 0;
+    public GameObject prefab;
+    public GameObject parent;
+    public int numberOfObjects = 64;
+    public AudioSource src;
+    public float radius = 5f;
+    public float scale = 100f;
+    public int range = 1024;
+    public int updateSpeed = 480;
+    public int rotateSpeed = 0;
 	
-	private GameObject[] cubes;
+    private GameObject[] cubes;
 
-	void Start ()
-	{
-		cubes = new GameObject[numberOfObjects];
-		for (int i = 0; i < numberOfObjects; i++) {
-			float angle = i * Mathf.PI * 2 / numberOfObjects;
-			Vector3 pos = new Vector3 (Mathf.Cos (angle), 0, Mathf.Sin (angle)) * radius;
-			GameObject tmp = Instantiate(prefab, pos, Quaternion.identity);
-			tmp.transform.parent = parent.transform;
-			cubes[i] = tmp;
-		}
-	}
+    void Start ()
+    {
+        cubes = new GameObject[numberOfObjects];
+        for (int i = 0; i < numberOfObjects; i++) {
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            Vector3 pos = new Vector3 (Mathf.Cos (angle), 0, Mathf.Sin (angle)) * radius;
+            GameObject tmp = Instantiate(prefab, pos, Quaternion.identity);
+            tmp.transform.parent = parent.transform;
+            cubes[i] = tmp;
+        }
+    }
 
-	void Update () {
-		float[] spectrum = new float[range];
-		src.GetSpectrumData (spectrum, 0, FFTWindow.Hanning);
-		for (int i = 0; i < cubes.Length; i++) {
-			Vector3 previousScale = cubes [i].transform.localScale;
-			previousScale.y = Mathf.Lerp (previousScale.y, spectrum [i] * scale, Time.deltaTime * updateSpeed);
-			cubes[i].transform.localScale = previousScale;
-			cubes[i].transform.position = new Vector3(cubes[i].transform.position.x, previousScale.y/2f,cubes[i].transform.position.z);
-			cubes[i].GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(0.67f-(spectrum [i]*3.5f), 1, 1));
-		}
-		for (int i = 0; i < numberOfObjects; i++) {
-			cubes[i].transform.Rotate (0, rotateSpeed, 0);		
-		}
-	}
+    void Update () {
+        float[] spectrum = new float[range];
+        src.GetSpectrumData (spectrum, 0, FFTWindow.Hanning);
+        for (int i = 0; i < cubes.Length; i++) {
+            Vector3 previousScale = cubes [i].transform.localScale;
+            previousScale.y = Mathf.Lerp (previousScale.y, spectrum [i] * scale, Time.deltaTime * updateSpeed);
+            cubes[i].transform.localScale = previousScale;
+            cubes[i].transform.position = new Vector3(cubes[i].transform.position.x, previousScale.y/2f,cubes[i].transform.position.z);
+            cubes[i].GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(0.67f-(spectrum [i]*3.5f), 1, 1));
+        }
+        for (int i = 0; i < numberOfObjects; i++) {
+            cubes[i].transform.Rotate (0, rotateSpeed, 0);		
+        }
+    }
 }
