100f6660a6ecad7e69434318c3004316eaec0568
Howard Tseng <howardt12345@gmail.com>
2020-01-26 10:00:53 -0500

    Newest version

--- ArSonora/Assets/Scripts/AudioVisualizer.cs
+++ ArSonora/Assets/Scripts/AudioVisualizer.cs
@@ -5,7 +5,8 @@ using System.Collections;
 public class AudioVisualizer : MonoBehaviour
 {
 	public GameObject prefab;
-	public int numberOfObjects = 20;
+	public GameObject parent;
+	public int numberOfObjects = 64;
 	public GameObject[] cubes;
 	public AudioSource src;
 	public float radius = 5f;
@@ -16,22 +17,25 @@ public class AudioVisualizer : MonoBehaviour
 
 	void Start ()
 	{
+		cubes = new GameObject[numberOfObjects];
 		for (int i = 0; i < numberOfObjects; i++) {
 			float angle = i * Mathf.PI * 2 / numberOfObjects;
 			Vector3 pos = new Vector3 (Mathf.Cos (angle), 0, Mathf.Sin (angle)) * radius;
-			Instantiate(prefab, pos, Quaternion.identity);
-			
+			GameObject cube = Instantiate(prefab, pos, Quaternion.identity);
+			cube.transform.parent = parent.transform;
+			cubes[i] = cube;
 		}
-		cubes = GameObject.FindGameObjectsWithTag("Cube");
 	}
 
 	void Update () {
 		float[] spectrum = new float[range];
-		src.GetSpectrumData (spectrum, 0, FFTWindow.Hamming);
+		src.GetSpectrumData (spectrum, 0, FFTWindow.Hanning);
 		for (int i = 0; i < cubes.Length; i++) {
 			Vector3 previousScale = cubes [i].transform.localScale;
 			previousScale.y = Mathf.Lerp (previousScale.y, spectrum [i] * scale, Time.deltaTime * updateSpeed);
 			cubes[i].transform.localScale = previousScale;
+			cubes[i].transform.position = new Vector3(cubes[i].transform.position.x, previousScale.y/2f,cubes[i].transform.position.z);
+			cubes[i].GetComponent<Renderer>().material.SetColor("_Color", Color.HSVToRGB(0.67f-(spectrum [i]*3.5f), 1, 1));
 		}
 		for (int i = 0; i < numberOfObjects; i++) {
 			cubes [i].transform.Rotate (0, rotateSpeed, 0);		
---
+++ ArSonora/Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TapToPlace : MonoBehaviour
+{
+    public GameObject visual;
+    public GameObject objectToPlace;
+    
+    private ARRaycastManager rayManager;
+    private ARSessionOrigin arOrigin;
+    private GameObject current;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        objectToPlace.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        //objectToPlace.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        arOrigin.MakeContentAppearAt(objectToPlace.transform, placementPose.position, placementPose.rotation);
+        objectToPlace.SetActive(true);
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        placementPoseIsValid = hits.Count > 0;
+        if(placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            visual.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+    }
+}
